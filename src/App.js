e26c4c3eba3a4fa3a0ee79b1c28b19afd092fe6f
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-04-30 23:25:20 -0400

    login register form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import LoginRegisterForm from './LoginRegisterForm'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    
+    this.state = {
+      loggedIn: false,
+    }
   }
+
   render() {
     return (
       <div className="App">
+        <LoginRegisterForm />
       </div>
     );
   }
---
+++ src/LoginRegisterForm/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Header, Button, Form, Checkbox, Divider, Grid, Segment } from 'semantic-ui-react';
+class LoginRegisterForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      register: false
+    }
+  }
+
+  switchLoginRegister = () => {
+    this.setState({ register: !this.state.register })
+  }
+
+  render() {
+    const LoginForm = (
+      <Form>
+        <Header size='medium' textAlign='center'>Login</Header>
+        <Form.Field>
+          <label>Username</label>
+          <input placeholder='Username' />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input placeholder='Password' type='password'/>
+        </Form.Field>
+        <Button type='submit'>Login</Button>
+      </Form>
+    )
+    const RegisterForm = (
+      <Form>
+        <Header size='medium' textAlign='center'>Register</Header>
+        <Form.Field>
+          <label>Username</label>
+          <input placeholder='Username' />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input placeholder='Password' type='password'/>
+        </Form.Field>
+        <Form.Field>
+          <label>Confirm Password</label>
+          <input placeholder='Re-enter Password' type='password'/>
+        </Form.Field>
+        <Button type='submit'>Register</Button>
+      </Form>
+    )
+    return (
+      <Segment placeholder>
+          <Grid columns={2} relaxed='very' stackable>
+            <Grid.Column>
+              {this.state.register ? RegisterForm : LoginForm}
+            </Grid.Column>
+
+            <Grid.Column verticalAlign='middle'>
+              <Button 
+              content={this.state.register ? 'Log in' : 'Register'} 
+              icon={this.state.register ? 'user' : 'signup'} 
+              size='big' 
+              onClick={this.switchLoginRegister}
+              />
+            </Grid.Column>
+          </Grid>
+          <Divider vertical>Or</Divider>
+        </Segment>
+    )
+  }
+}
+
+export default LoginRegisterForm
